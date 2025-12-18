fef3bfe34f9c722bc3b40016c346a15434734758
Lubabah Pervaj <lubabah.p@outlook.com>
2025-12-18 10:44:52 +0000

    Implement create_key method for key generation
    Added create_key method to generate encryption key.

--- passwordmanager.py
+++ passwordmanager.py
@@ -10,4 +10,12 @@ class PasswordManager:
     self.key = None   # the self thing attaches this specific key to the object and remembers it forever
     self.password_file = None 
     self.password_dict = {}    # obviously the password dictionary
+    # i still kinda don't understand constructors but i guess i will when i run it
+  
+  def create_key(self, path):    # path to store the key into a file
+    self.key = Fernet.generate_key()   # key can be used for encryption and decryption 
+    
+    
+
+     
 
